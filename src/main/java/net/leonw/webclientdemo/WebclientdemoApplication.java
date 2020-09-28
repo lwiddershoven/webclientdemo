81f3650c4833b5deb283b57edb801a6c281404d0
Leon <qaz@dds.nl>
2020-09-28 13:14:03 +0200

    start.spring.io

---
+++ src/main/java/net/leonw/webclientdemo/WebclientdemoApplication.java
@@ -0,0 +1,13 @@
+package net.leonw.webclientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebclientdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebclientdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/leonw/webclientdemo/WebclientdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leonw.webclientdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebclientdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
