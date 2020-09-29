c35a6dc818dd4f210917f31b4afbc42d2e5e4637
Leon <qaz@dds.nl>
2020-09-29 17:34:07 +0200

    Cleaned up imports

--- src/main/java/net/leonw/webclientdemo/CWithRetriesController.java
+++ src/main/java/net/leonw/webclientdemo/CWithRetriesController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.retry.Retry;
